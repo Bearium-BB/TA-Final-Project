91b8a544eadde20d0ef8c623313f1a88801725dd
Bearium-BB <brett200410@gmail.com>
2022-05-18 23:31:32 -0500

    styling updating

--- js/contact-us.js
+++ js/contact-us.js
@@ -20,3 +20,60 @@ const map = new mapboxgl.Map({
     interactive: false,
     zoom: 17 // starting zoom
 });
+
+// Create a new marker.
+const marker = new mapboxgl.Marker()
+    .setLngLat([-97.1750196, 49.8482787])
+    .addTo(map);
+
+
+
+const $form = document.getElementById('form')
+const $firstName = document.getElementById('first-name')
+const $lastName = document.getElementById('last-name')
+const $age = document.getElementById('age')
+const $email = document.getElementById('email')
+const $submit = document.getElementById('submit')
+const $mes = document.getElementById('ER')
+
+const emailPattern = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/
+$submit.addEventListener('click', () => {
+    let firstName = $firstName.value.trim();
+    let email = $email.value.trim();
+
+    let message = '';
+    let valid = true;
+    let count = 0;
+
+    if (firstName === '') {
+        message += 'First name is required and'
+        valid = false;
+        count++;
+    }
+
+    if (email === '') {
+        message += ' email is required\n';
+        valid = false;
+        count++;
+
+    } else {
+        if (!emailPattern.test(email)) {
+            message += ' email is not valid';
+            valid = false;
+        }
+    }
+    if (count === 3) {
+        ER.style.color = 'green'
+        ER.innerText = 'Message Sent'
+        ER.style.opacity = 1
+    } else if (!valid) {
+        ER.style.color = 'red'
+        ER.innerText = message
+        ER.style.opacity = 1
+
+    } else {
+        ER.style.color = 'green'
+        ER.innerText = 'Message Sent'
+        ER.style.opacity = 1
+    }
+})
